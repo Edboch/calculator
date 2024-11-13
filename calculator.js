3df75e9886790f4921349260fae1f9ed0491e335
Edmond <edmondchu10@hotmail.com>
2024-11-13 19:47:02 +0000

    Add calculation feature
    Calculator will accept sequences of calculations and be able to evaluate the
    question and display an answer.

--- calculator.js
+++ calculator.js
@@ -1,14 +1,53 @@
 const display = document.querySelector('#display');
 const buttons = document.querySelectorAll('button');
+let resultHolder = null;
+let operator = null;
+let secondValStarted = false;
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        if (button.textContent === 'C') {
-            display.innerHTML = '';
-        } else if (button.textContent === '=') {
-            console.log('Not implemented');
-        } else {
-            display.textContent+=button.textContent;
+        let content = button.textContent;
+        switch (content) {
+            case 'C':
+                display.innerHTML = '0';
+                resultHolder = null;
+                operator = null;
+                secondValStarted = false;
+                break
+            case '=':
+                let secondNum = parseInt(display.textContent);
+                let result = operate(operator,resultHolder,secondNum);
+                display.textContent = (isNaN(result))?'Haha':result;
+                resultHolder = null;
+                operator = null;
+                secondValStarted = false;
+                break
+            case '+':
+            case '-':
+            case '/':
+            case '*':
+                if (resultHolder) {
+                    // User has pressed the operator button for the second time
+                    // this case evaluates the first calculation
+                    // e.g 2+2+ --> 10+
+                    let secondNum = parseInt(display.textContent);
+                    resultHolder = operate(operator,resultHolder,secondNum)
+                    display.textContent = resultHolder;
+                    secondValStarted = false;
+                } else {
+                    resultHolder = parseInt(display.textContent);
+                }
+                operator = content;
+                break
+            default:
+                if (operator && !secondValStarted){
+                    display.textContent = content;
+                    secondValStarted = true;
+                } else if (display.textContent === '0') {
+                    display.textContent = content;
+                } else {
+                    display.textContent+=content;
+                }
         }
     })
 })
@@ -27,24 +66,26 @@ function multiply(number1,number2) {
 }
 
 function divide(number1,number2) {
-    return number1/number2;
+    return number2!==0?number1/number2:NaN;
 }
 
 function operate(op,number1,number2) {
+    let result;
     switch (op) {
         case '+':
-            add(number1,number2);
+            result = add(number1,number2);
             break
         case '-':
-            subtract(number1,number2);
+            result = subtract(number1,number2);
             break
         case '*':
-            multiply(number1,number2);
+            result = multiply(number1,number2);
             break
         case '/':
-            divide(number1,number2);
+            result = divide(number1,number2);
             break
         default:
-            console.warn('The operator is invalid!')
+            console.warn('One of the parameters are missing!')
     }
+    return result;
 }
